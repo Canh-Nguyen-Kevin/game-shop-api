d159c33b64137a28b083393e98dbc493ef509397
= <=>
2022-02-26 11:11:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const importData = require("./db.json");
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/products", (req, res) => {
+  res.send(importData.products);
+});
+app.listen(port, () => {
+  console.log(`Example app is listening on  port https://localhost:${port}`);
+});
