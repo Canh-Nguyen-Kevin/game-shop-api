41bd2ab846e7e0b652e9c37e0a231b5ca5fdf8ba
= <=>
2022-02-27 13:43:30 +0700

    vvv

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.get("/", (req, res) => {
 app.get("/products", (req, res) => {
   res.send(importData.products);
 });
+app.get("/products/id", (req, res) => {
+  res.send(importData.products.id);
+});
 app.listen(port, () => {
   console.log(`Example app is listening on  port https://localhost:${port}`);
 });
